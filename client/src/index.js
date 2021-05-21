1a6266d33e3fac429aa461d317dfdad33eeaf669
jnunez1229 <70110259+jnunez1229@users.noreply.github.com>
2021-05-20 19:47:21 -0500

    added reducer, devtools ext, and Provider to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {createStore, combineReducers} from 'redux';
+import {createStore} from 'redux';
 import reducer from './utils/reducers'
+import {Provider} from 'react-redux'
 
-const store = createStore(reducer)
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
